5feecd51bdee02c143cabb29c313e91ddfcb5605
Michael Zeidlewicz <154428697+Schmusebaum@users.noreply.github.com>
2024-07-01 10:48:57 +0000

    02 / 03 Datentypen

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -1,8 +1,29 @@
 //console.log("Hi");
 //console.log(Hi);
-
+/*
 let firstName; // Deklaration (Definition)
 firstName = "Max";
 //console.log(firstName); // Ausgabe
 let familyName = "Mütze";
 console.log("Hi, ich bin " + firstName + " " + familyName + "!");
+*/
+
+let test; // Deklaration
+
+console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+console.log("Inhalt: " + test);             //Ausgabe Inhalt
+console.log("-----------------");
+
+test = "hi"; //string
+console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+console.log("Inhalt: " + test);             //Ausgabe Inhalt
+console.log("-----------------");
+test = 2; // number
+console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+console.log("Inhalt: " + test);             //Ausgabe Inhalt
+console.log("-----------------");
+
+test = true; // boolean / bool
+console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+console.log("Inhalt: " + test);             //Ausgabe Inhalt
+console.log("-----------------");
