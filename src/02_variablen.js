fb8c1d921b8c779e8a27a8cef7b3b2503e6df191
Michael Zeidlewicz <154428697+Schmusebaum@users.noreply.github.com>
2024-07-01 10:57:13 +0000

    Konstante

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -8,7 +8,7 @@ let familyName = "Mütze";
 console.log("Hi, ich bin " + firstName + " " + familyName + "!");
 */
 
-let test; // Deklaration
+/*let test; // Deklaration
 
 console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
 console.log("Inhalt: " + test);             //Ausgabe Inhalt
@@ -27,3 +27,16 @@ test = true; // boolean / bool
 console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
 console.log("Inhalt: " + test);             //Ausgabe Inhalt
 console.log("-----------------");
+*/
+
+
+/* Variablen vs. Konstanten in JS */
+
+// Variable
+let test; // Variable : Deklaration
+test = "hi"; // Wertzuweisung (Initialisierung)
+test = "hello"; // Überschreiben
+console.log("Inhalt: " + test); // Ausgabe 
+
+const test2 = "Hi!";
+console.log(test2);
