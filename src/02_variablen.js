f64662231f9d7d7b35b900f4b314e4f517e46eb8
Michael Zeidlewicz <154428697+Schmusebaum@users.noreply.github.com>
2024-07-01 10:25:26 +0000

    02 / 02 Deklaration und Wertzuweisung

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -1,9 +1,8 @@
-console.log("Hi");
-/**
- * 
- * 
- * 
- * 
- * 
- * 
- */
+//console.log("Hi");
+//console.log(Hi);
+
+let firstName; // Deklaration (Definition)
+firstName = "Max";
+//console.log(firstName); // Ausgabe
+let familyName = "Mütze";
+console.log("Hi, ich bin " + firstName + " " + familyName + "!");
