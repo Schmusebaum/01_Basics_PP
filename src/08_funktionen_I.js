37c8590965cb9ebb2f24afc398398f1321f0418f
Michael Zeidlewicz <154428697+Schmusebaum@users.noreply.github.com>
2024-07-04 08:51:45 +0000

    08 / 02 Parametrisierung + Datenübergabe von INNEN

---
+++ src/08_funktionen_I.js
@@ -0,0 +1,30 @@
+
+/***** Funktionen 01 *****/
+// 1. Kapselung von Codeblöcken
+
+// Funktionsaufruf | call
+//test();
+
+
+// Funktionsrumpf | callee
+// Funktionsdeklaration
+function test()
+{
+    console.log("Hallo, Marius! ")
+}
+
+/***** Funktionen 02a *****/
+// 2a. Parametrisierung + Datenübergabe von INNEN
+
+// Call
+ausgabeNamen();
+
+// Funktion
+
+function ausgabeNamen() {
+    // interne Variable | What happens in VEGAS ...
+    let firstName = "Sara";
+    console.log("Hallo, " + firstName + "!");
+    
+}
+//console.log(firstName); //Fehler: scope!
